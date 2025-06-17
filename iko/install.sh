329e5a9f0abf71f32a382ccf4f72cb12233d13cb
Beau Barker <beau@explodinglabs.com>
2025-06-17 10:50:26 +1000

    Fix install.sh

--- iko/install.sh
+++ iko/install.sh
@@ -17,14 +17,15 @@ ENV_ARG=""
 [ -f .env ] && ENV_ARG="--env-file .env"
 
 IKORC_ARG=""
-[ -f .ikorc ] && IKORC_ARG="-v ${PWD}/.ikorc:/home/.ikorc:ro"
+IKORC_PATH="${IKORC_PATH:-$HOME/.config/ikorc}"
+[ -f "$IKORC_PATH" ] && IKORC_ARG="-v $IKORC_PATH:/home/.ikorc:ro"
 
 docker run --rm -it \
   $ENV_ARG \
   $IKORC_ARG \
   --network "${DOCKER_NETWORK:-bridge}" \
   -v "${PWD}/migrations:/repo:rw" \
-  -v "${PWD}/scripts:/iko/scripts:ro" \
+  -v "${PWD}/scripts:/scripts:ro" \
   ghcr.io/explodinglabs/iko:0.1.0 "$@"
 EOF
 
